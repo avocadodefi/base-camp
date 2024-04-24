2058ace6fa20c92ce6ea9de862404c15947075fc
0xDev <84038223+avocadodefi@users.noreply.github.com>
2024-04-24 03:16:46 +0300

    Create control-structures.sol

---
+++ control-structures.sol
@@ -0,0 +1,39 @@
+https://docs.base.org/base-camp/docs/control-structures/control-structures-exercise/
+
+CODE:
+
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract ControlStructures {
+    error AfterHours(uint _time);
+
+    function fizzBuzz(uint _number) external pure returns (string memory) {
+        if (_number % 3 == 0) {
+            if (_number % 5 == 0) {
+               return "FizzBuzz";
+            }
+            return "Fizz";
+        } else if (_number % 5 == 0) {
+            return "Buzz";
+        }
+        return "Splat";
+    }
+
+    function doNotDisturb(uint _time) external pure returns (string memory) {
+        assert(_time < 2400);
+        if (_time > 2200 || _time < 800) {
+            revert AfterHours(_time);
+        }
+        if (_time > 800 && _time < 1199) {
+            return "Morning!";
+        }
+        if (_time > 1200 && _time < 1259) {
+            revert("At lunch!");
+        }
+        if (_time > 1300 && _time < 1799) {
+            return "Afternoon!";
+        }
+        return "Evening!";
+    }
+}
